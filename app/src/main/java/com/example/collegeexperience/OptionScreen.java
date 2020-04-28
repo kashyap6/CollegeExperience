2741ccfacf7f4c26cf6f56dc56bef3652b26c214
rohanjt2 <rohanjt2@illinois.edu>
2020-04-28 00:19:45 -0500

    Buttons move you around interface

--- app/src/main/java/com/example/collegeexperience/OptionScreen.java
+++ app/src/main/java/com/example/collegeexperience/OptionScreen.java
@@ -2,7 +2,9 @@ package com.example.collegeexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OptionScreen extends AppCompatActivity {
 
@@ -10,5 +12,58 @@ public class OptionScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option_screen);
+        Button backToMain = findViewById(R.id.btn_dropOut);
+        backToMain.setOnClickListener (unused -> {
+            finish();
+        });
+        Button buy = findViewById(R.id.btn_buyTextBook);
+        buy.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button studyFriends = findViewById(R.id.btn_studyFriends);
+        studyFriends.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button redLion = findViewById(R.id.btn_redLion);
+        redLion.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button studyAlone = findViewById(R.id.btn_studyAlone);
+        studyAlone.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button sleep = findViewById(R.id.btn_sleep);
+        sleep.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button excersie = findViewById(R.id.btn_exercise);
+        excersie.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button mckinley = findViewById(R.id.btn_mckinley);
+        mckinley.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+        Button work = findViewById(R.id.btn_work);
+        work.setOnClickListener (unused -> {
+            Intent UpdateScreen = new Intent(this, UpdateScreen.class);
+            startActivity(UpdateScreen);
+            finish();
+        });
+
     }
 }
--- app/src/main/java/com/example/collegeexperience/UpdateScreen.java
+++ app/src/main/java/com/example/collegeexperience/UpdateScreen.java
@@ -2,7 +2,9 @@ package com.example.collegeexperience;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UpdateScreen extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class UpdateScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_screen);
+        Button next = findViewById(R.id.btn_Proceed);
+        next.setOnClickListener (unused -> {
+            Intent OptionScreen = new Intent(this, OptionScreen.class);
+            startActivity(OptionScreen);
+            finish();
+        });
     }
 }
