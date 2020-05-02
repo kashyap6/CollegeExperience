290f474bf611f69a44138bd8cb3e9e355b5f9a29
rohanjt2 <rohanjt2@illinois.edu>
2020-05-02 00:56:33 -0500

    Now has Background music that can be turned on and off.

---
+++ app/src/main/java/com/example/collegeexperience/BackgroundMusicService.java
@@ -0,0 +1,30 @@
+package com.example.collegeexperience;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class BackgroundMusicService extends Service {
+    private MediaPlayer backingMusic;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        backingMusic = MediaPlayer.create(this, R.raw.head_candy);
+        backingMusic.setLooping(true);
+        backingMusic.start();
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        backingMusic.stop();
+    }
+}
--- app/src/main/java/com/example/collegeexperience/MainMenu.java
+++ app/src/main/java/com/example/collegeexperience/MainMenu.java
@@ -12,14 +12,27 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        // music
+        startService(new Intent(this, BackgroundMusicService.class));
+        Button music = findViewById(R.id.btn_music);
+        music.setOnClickListener (unused -> {
+            if (music.getText().equals("Music On")) {
+                stopService(new Intent(this, BackgroundMusicService.class));
+                music.setText("Music Off");
+            } else if (music.getText().equals("Music Off")) {
+                startService(new Intent(this, BackgroundMusicService.class));
+                music.setText("Music On");
+            }
+        });
         Button gameStart = findViewById(R.id.startButton);
         gameStart.setOnClickListener (unused -> {
             Intent optionScreen = new Intent(this, OptionScreen.class);
-            TurnCalculation thisGame = new TurnCalculation();
+            //TurnCalculation thisGame = new TurnCalculation();
             startActivity(optionScreen);
         });
         Button leaveGame = findViewById(R.id.exitButton);
         leaveGame.setOnClickListener (unused -> {
+            stopService(new Intent(this, BackgroundMusicService.class));
             finish();
         });
     }
